f600f65fc700683291733f27029207bdddc536de
Mokalol25 <93078882+Mokalol25@users.noreply.github.com>
2024-03-06 17:45:31 +0000

    Deploying to gh-pages from @ Tate-T/JS-games-project@80a412cb6a598e3ccc76099deecb5fcb41e9f378 🚀

---
+++ index.aa8158ae.js
@@ -0,0 +1,2 @@
+document.querySelector(".guess-the-number__title"),document.querySelector(".guess-the-number__input"),document.querySelector(".guess-the-number__button"),document.querySelector(".guess-the-number__svg"),document.querySelector(".guess-the-number__text");
+//# sourceMappingURL=index.aa8158ae.js.map
---
+++ index.ba41f159.js
@@ -0,0 +1,2 @@
+document.querySelector(".guess-the-number__title"),document.querySelector(".guess-the-number__input"),document.querySelector(".guess-the-number__button"),document.querySelector(".guess-the-number__svg"),document.querySelector(".guess-the-number__text");
+//# sourceMappingURL=index.ba41f159.js.map
